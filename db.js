52dce12d0473f8429b5de4c47749d4fc971bc1ed
gaurav <gauravbokaro1@gmail.com>
2022-05-31 23:16:45 +0530

    my project

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheyjobsudemy' , {useNewUrlParser : true});
+         await mongoose.connect('mongodb+srv://gaurav121:gaurav121@cluster0.hgtx53y.mongodb.net/?retryWrites=true&w=majority' , {useNewUrlParser : true});
          console.log('Mongo DB Connection success')
      } catch (error) {
          console.log('Mongo DB Connection failed')
